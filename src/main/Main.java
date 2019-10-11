e73fd32f8da503c9753a3a520889f6b6c1364bf7
User <User@192.168.0.240>
2019-10-11 14:31:30 +0200

    This is homework 1.

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import calculator.Calculator;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args) {
+       Calculator cal = new Calculator();
+        
+    }
+}
